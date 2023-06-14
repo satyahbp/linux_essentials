6298d95787900e15f4463c1b9c33ac64483d170e
satyajeet <satya.hbp@gmail.com>
2023-06-15 00:57:13 +0530

    added something for fedora

---
+++ fedora/vbox_essestials.sh
@@ -0,0 +1,6 @@
+# install these things to run vbox properly
+sudo dnf install gcc
+sudo dnf install perl
+# secure boot has to be disabled after this
+sudo dnf install kernel-devel
+# whatever else is written in /sbin/vboxconfig file
