320c7ecb2d1d35dc708abbaff74c5f25ba5aadd6
satyahbp <satya.hbp@gmail.com>
2023-09-02 22:07:50 +0530

    added further updates

--- debian/essentials.sh
+++ debian/essentials.sh
@@ -3,3 +3,10 @@ sudo apt install git
 sudo apt install curl
 sudo apt install openssh-server
 sudo apt install openssh-client
+sudo apt install gcc
+sudo apt install g++
+sudo apt install cmake
+sudo apt install build-essential
+sudo apt install clang
+sudo apt install libssl-dev libcurl4-openssl-dev
+sudo apt install transmission
