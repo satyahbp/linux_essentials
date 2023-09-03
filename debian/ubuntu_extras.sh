adf947724ba5f806e37a0568420864ba87f7868e
satyahbp <satya.hbp@gmail.com>
2023-09-03 11:58:14 +0530

    added ubuntu extras package command

---
+++ debian/ubuntu_extras.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# ubuntu or ubuntu based distros sometimes may not be able to play videos properly
+# because of lack of proper codecs. This command solves the problem to some extent
+sudo apt install ubuntu-restricted-extras
