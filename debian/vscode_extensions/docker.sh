6c7dba7cb3a37f63422a32053d6896deb5c009f3
satyahbp <satya.hbp@gmail.com>
2023-09-09 18:35:42 +0530

    added vs code extension for docker

---
+++ debian/vscode_extensions/docker.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# this will install the following vs-code extensions:
+# 1. Docker
+
+echo "Installing the following extension:"
+echo "1. Docker"
+code --install-extension ms-azuretools.vscode-docker
